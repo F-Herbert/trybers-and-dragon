b49595cbac78808a932e0315095f105f5c073977
felipe <felipe_herbert@hotmail.com.br>
2023-01-24 20:15:10 -0300

    Requisito 11 concluído

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,22 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Monster from '../Monster';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(private character: 
+  Fighter, private monster:Monster[] | 
+  SimpleFighter[] | 
+  Fighter[]) {
+    super(character);
+  }
+
+  fight(): number {
+    this.monster.forEach((monster) => {
+      if (this.character.lifePoints > 0 && monster.lifePoints > 0) {
+        this.character.attack(monster);
+        monster.attack(this.character);
+      }
+    });
+    return this.character.lifePoints === -1 ? -1 : 1;
+  }
+}
